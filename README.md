e0d8ef6acdb81d7b8a4ec06149f4d765ad62c085
Matthew Gyurgyik <matthew@gyurgyik.io>
2020-12-12 15:41:54 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# OpenWrt A/B Upgrades
+## Overview
+A/B upgrades allow for safer OpenWrt upgrades. If the upgrade does not go as
+planned, you can easily boot into your previous image.
+
+The A/B upgrade approach functions by having two root parititons. When
+executing an upgrade the new image will be written to the non-active root
+partition.
+
+### Partition Layout
+/dev/sda1 = /boot
+/dev/sda2 = rootfs_a
+/dev/sda3 = rootfs_b
+
+
+### High Level Process
+1. Detect non-active partition
+2. Create sysupgrade backup
+3. Format partition
+4. Mount partition to /mnt/sysupgrade
+5. Extract rootfs to /mnt/sysupgrade
+6. Copy kernel to /mnt/sysupgrade/vmlinuz
+7. Extract openwrt-backup to /mnt/sysupgrade
+8. Backup grub.cfg
+9. Generate new grub.cfg
+10. Reboot 
+
+## Setup
+
+1. Install Packages: `opkg install fdisk coreutils-stat blkid e2fsprogs`
+2. Create partiton for second rootfs `fdisk /dev/sda`
+    ```
+    root@OpenWrt:~# fdisk /dev/sda
+
+    Command (m for help): p
+    Disk /dev/sda: 8 GiB, 8589934592 bytes, 16777216 sectors
+    Device     Boot Start    End Sectors  Size Id Type
+    /dev/sda1  *      512  33279   32768   16M 83 Linux
+    /dev/sda2       33792 558079  524288  256M 83 Linux
+
+    Command (m for help): n
+    Select (default p): p
+    Partition number (3,4, default 3): 3
+    First sector (33280-16777215, default 559104):
+    Last sector, +/-sectors or +/-size{K,M,G,T,P} (559104-16777215, default 16777215): +256M
+
+    Created a new partition 3 of type 'Linux' and of size 256 MiB.
+
+    Command (m for help): w
+    The partition table has been altered.
+    Syncing disks.
+    ```
+3. Copy current kernel to rootfs: `cp /boot/vmlinuz /vmlinuz`
+
+## Upgrading
+1. Modify upgrade.sh
+    * Update `BOOT_DEV`, `ROOTA_DEV`, `ROOTB_DEV`
+    * Update default grub options 
+2. Install Packages: `opkg install coreutils-stat blkid`
+3. Download rootfs and kernel to /root
+     ```
+     wget https://downloads.openwrt.org/snapshots/targets/x86/64/openwrt-x86-64-generic-kernel.bin
+     wget https://downloads.openwrt.org/snapshots/targets/x86/64/openwrt-x86-64-rootfs.tar.gz
+     ```
+3. Upgrade: `./upgrade.sh openwrt-x86-64-generic-kernel.bin openwrt-x86-64-rootfs.tar.gz`
+4. Reboot
+
---
+++ upgrade.sh
@@ -0,0 +1,180 @@
+#!/bin/sh
+
+BOOT_DEV="/dev/sda1"
+ROOTA_DEV="/dev/sda2"
+ROOTB_DEV="/dev/sda3"
+
+# Arguments
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 kernel rootfs"
+    exit 1
+fi
+
+if [ -z $1 ]; then
+    echo "Argument 1 must be the kernel!"
+    exit 1
+fi
+
+if [ -z $2 ]; then
+    echo "Argument 2 must be the rootfs!"
+    exit 1
+fi
+
+KERNEL=$1
+ROOTFS=$2
+
+# Make sure files exist
+if [ ! -f $KERNEL ]; then
+    echo "$KERNEL does not exist!"
+    exit 1
+fi
+if [ ! -f $ROOTFS ]; then
+    echo "ROOTFS does not exist!"
+    exit 1
+fi
+
+# rootfs needs to be gzipped tarball
+if ! gzip -t $ROOTFS; then
+    echo "rootfs is not a gzipped file! Abort!"
+    exit 1
+fi
+
+
+# Require stat
+if ! which stat &>/dev/null; then
+    echo "stat is required: opkg install coreutils-stat"
+    exit 1
+fi
+
+# Require mkfs.ext4
+if ! which mkfs.ext4 &>/dev/null; then
+    echo "mkfs.ext4 is required: opkg install e2fsprogs"
+    exit 1
+fi
+
+# Require blkid
+if ! which blkid &>/dev/null; then
+    echo "blkid is required: opkg install blkid"
+    exit 1
+fi
+
+# If this exists, we probably have an incomplete upgrade
+if [ -d /mnt/sysupgrade ]; then
+    echo "/mnt/sysupgrade exists! Unexpected, aborting!"
+    exit 1
+fi
+
+
+# Check if specified boot volume exists and is mounted on /boot
+if [ -b $BOOT_DEV ]; then
+    bootIdByBlock=$(stat -c "%02t%02T" $BOOT_DEV)
+    bootIdByPath=$(stat -c %04D /boot)
+    if [ $bootIdByBlock != $bootIdByPath ]; then
+        echo "/boot is not mounted on $BOOT_DEV! Abort!"
+        exit 1
+    fi
+else
+    echo "boot device, $BOOT_DEV, does not exist! Abort!"
+    exit 1
+fi
+
+# Get Root Volume
+#ROOT_DEV=$(findmnt -n -o SOURCE /)
+
+# Major Minor
+rootIdByDev=$(stat -c %04D /)
+rootOneIdByBlock=$(stat -c "%02t%02T" $ROOTA_DEV)
+rootTwoIdByBlock=$(stat -c "%02t%02T" $ROOTB_DEV)
+
+if [ $rootIdByDev = $rootOneIdByBlock ]; then
+    ROOT_DEV=$ROOTA_DEV
+    TARGET_DEV=$ROOTB_DEV
+    LABEL="rootfs_b"
+elif [ $rootIdByDev = $rootTwoIdByBlock ]; then
+    ROOT_DEV=$ROOTB_DEV
+    TARGET_DEV=$ROOTA_DEV
+    LABEL="rootfs_a"
+else
+    echo "rootfs is not mounted on $ROOTA_DEV or $ROOTB_DEV! Abort!"
+    exit 1
+fi
+
+# Exit if target device is mounted
+if mount $TARGET_DEV &>/dev/null; then
+    echo "Target device, $TARGET_DEV, is mounted! Abort!"
+    exit 1
+fi
+
+# Print some information
+echo "Current Root FS: $ROOT_DEV"
+echo "Target Root FS: $TARGET_DEV"
+echo "Kernel: $KERNEL"
+echo "Rootfs: $ROOTFS"
+
+# Time to be destructive
+echo -n "Continue? (y/n) "
+read answer
+
+if ! ([ $answer == Y ] || [ $answer == y ]); then
+    echo "Aborting!"
+    exit 1
+fi
+
+# Do a backup
+sysupgrade -b openwrt-backup.tar.gz
+if [ $? != 0 ]; then
+    echo "sysupgrade backup failed! Abort!"
+    exit 1
+fi
+
+
+echo "Formatting $TARGET_DEV..."
+mkfs.ext4 -L $LABEL -b 4096 -m 0 $TARGET_DEV
+
+echo "Mounting $TARGET_DEV to /mnt/sysupgrade..."
+mkdir /mnt/sysupgrade
+mount $TARGET_DEV /mnt/sysupgrade || exit 1
+
+echo "Extracting rootfs..."
+zcat $ROOTFS | tar x -C /mnt/sysupgrade || exit 1
+
+echo "Copying kernel..."
+cp $KERNEL /mnt/sysupgrade/vmlinuz || exit 1
+
+echo "Restoring backup..."
+zcat openwrt-backup.tar.gz | tar x -C /mnt/sysupgrade || exit 1
+
+echo "Generating grub.cfg..."
+cp /boot/grub/grub.cfg /boot/grub/grub.cfg.bak
+
+release=$(grep OPENWRT_RELEASE /mnt/sysupgrade/etc/os-release | cut -d'"' -f2)
+partuuid=$(blkid -o export $TARGET_DEV | grep PARTUUID | cut -d'=' -f2)
+partid=$(echo $TARGET_DEV | egrep -o '[0-9]+')
+a_release=$(grep OPENWRT_RELEASE /etc/os-release | cut -d'"' -f2)
+a_partuuid=$(blkid -o export $ROOT_DEV | grep PARTUUID | cut -d'=' -f2)
+a_partid=$(echo $ROOT_DEV | egrep -o '[0-9]+')
+
+cat <<EOF > /boot/grub/grub.cfg
+serial --unit=0 --speed=115200 --word=8 --parity=no --stop=1 --rtscts=off
+terminal_input console serial; terminal_output console serial
+
+set default="0"
+set timeout="5"
+set root='(hd0,msdos1)'
+
+menuentry "$release" {
+	linux (hd0,msdos${partid})/vmlinuz root=PARTUUID=$partuuid rootwait  console=tty0 console=ttyS0,115200n8 noinitrd
+}
+menuentry "$release (failsafe)" {
+	linux (hd0,msdos${partid})/vmlinuz failsafe=true root=PARTUUID=$partuuid rootwait  console=tty0 console=ttyS0,115200n8 noinitrd
+}
+menuentry "$a_release" {
+	linux (hd0,msdos${a_partid})/vmlinuz root=PARTUUID=$a_partuuid rootwait  console=tty0 console=ttyS0,115200n8 noinitrd
+}
+menuentry "$a_release (failsafe)" {
+	linux (hd0,msdos${a_partid})/vmlinuz failsafe=true root=PARTUUID=$a_partuuid rootwait  console=tty0 console=ttyS0,115200n8 noinitrd
+}
+EOF
+
+
+umount /mnt/sysupgrade
